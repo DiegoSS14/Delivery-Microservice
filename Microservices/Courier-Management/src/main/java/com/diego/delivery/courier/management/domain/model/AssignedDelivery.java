18bd262009349b605dcb2dd6a5af7f311aa1c6fe
DiegoSS14 <sousadiego533@gmail.com>
2026-01-03 00:51:15 -0300

    feat: Add assign and fulFill methods in Courier

--- Microservices/Courier-Management/src/main/java/com/diego/delivery/courier/management/domain/model/AssignedDelivery.java
+++ Microservices/Courier-Management/src/main/java/com/diego/delivery/courier/management/domain/model/AssignedDelivery.java
@@ -1,5 +1,29 @@
 package com.diego.delivery.courier.management.domain.model;
 
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Setter(AccessLevel.PRIVATE)
 public class AssignedDelivery {
-    
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    private OffsetDateTime assignedAt;
+
+    static AssignedDelivery pending(UUID deliveryId) {
+        AssignedDelivery delivery = new AssignedDelivery();
+        delivery.setId(deliveryId);
+        delivery.setAssignedAt(OffsetDateTime.now());
+        return delivery;
+    }
 }
