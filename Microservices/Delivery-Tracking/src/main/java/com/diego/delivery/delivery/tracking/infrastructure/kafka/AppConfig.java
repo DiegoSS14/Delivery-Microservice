aa78a2b5b877cebe7e23f5dd3043ba2020091222
DiegoSS14 <sousadiego533@gmail.com>
2026-02-03 20:16:29 -0300

    feat: configure bens of KafkaTemplate and ProducerFactory in AppConfig class and add jackson and jsr310 in pom.xml

---
+++ Microservices/Delivery-Tracking/src/main/java/com/diego/delivery/delivery/tracking/infrastructure/kafka/AppConfig.java
@@ -0,0 +1,38 @@
+package com.diego.delivery.delivery.tracking.infrastructure.kafka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+@Configuration
+public class AppConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> kafkaProducerFactory() {
+        Map<String, Object> producerProperties = new HashMap<>();
+        producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        producerProperties.put(ProducerConfig.ACKS_CONFIG, "all");
+        producerProperties.put(ProducerConfig.RETRIES_CONFIG, 3);
+        producerProperties.put(ProducerConfig.LINGER_MS_CONFIG, 10);
+        
+        return new DefaultKafkaProducerFactory<>(producerProperties);
+    }
+}
